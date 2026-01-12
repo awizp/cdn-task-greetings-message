59c2abe7efc0de4b98b2a144e4e96d4ab66a1f82
Awizp <ridvishnu@gmail.com>
2026-01-12 12:42:35 +0530

    fix: major changes in UI

--- src/main.js
+++ src/main.js
@@ -5,16 +5,6 @@ let buttonEl = document.getElementById("btn");
 let errorEl = document.getElementById("errorMessage");
 let bodyEl = document.body;
 
-const randomColorGenerator = () => {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        // color value by multiplying random number with letters length,
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-};
-
 buttonEl.addEventListener('click', () => {
 
     // name validation by regular expression,
@@ -23,16 +13,11 @@ buttonEl.addEventListener('click', () => {
     if (regEx.test(inputEl.value)) {
         errorEl.style.display = "none";
 
-        // getting color value,
-        const colorValue = randomColorGenerator();
-
-        // setting uppercase to value,
-        let value = inputEl.value.toLocaleUpperCase();
+        // input value,
+        let value = inputEl.value;
 
         // setting color values to the elements,
         headingEl.innerHTML = `Hello ${value}, Welcome to CyberDude Networks!`;
-        headingEl.style.color = colorValue;
-        bodyEl.style.background = colorValue;
         inputEl.value = "";
     } else {
         errorEl.style.display = "block";
